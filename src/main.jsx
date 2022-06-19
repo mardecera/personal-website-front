17b08aa83b2fbf4a416332f9e4834cab59d87404
mardecera <mardecera.personal@gmail.com>
2022-06-19 14:39:07 -0500

    My first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import NotFound from './views/NotFound'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+)
---
+++ src/views/NotFound/index.jsx
@@ -0,0 +1,7 @@
+import styles from './index.module.css'
+
+const NotFound = () => {
+    return <div className={styles.notFound}>404 Not Found</div>
+}
+
+export default NotFound
